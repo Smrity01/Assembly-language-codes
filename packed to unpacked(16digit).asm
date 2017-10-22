674cbce4a71068f9e37e97bf212589db153e7670
Smrity chaudhary <smrity.mca17.du@gmail.com>
2017-10-22 17:08:13 +0530

    packed to unpacked BCD conversion

---
+++ packed to unpacked(16digit).asm
@@ -0,0 +1,28 @@
+data segment
+    packed db 21h,34h,56h,78h,49h,17h,63h,82h
+    unpacked dw ?,?,?,?,?,?,?,?
+data ends
+code segment
+    assume: DS:data CS:code
+    start:mov ax,data
+          mov ds,ax
+          mov cx,8
+          mov ax,0
+          mov si,0
+          mov di,0 
+          
+     next:mov ax,0
+          mov al,packed[si]
+          shl ax,4
+          shr al,4
+          mov unpacked[di],ax
+          inc si
+          inc di
+          inc di
+          loop next
+code ends
+end start
+
+
+
+
